6b238139d407b60d730a4da941fbd84ec4938f78
RockRockWhite <1834309151@qq.com>
2024-03-11 21:46:55 +0800

    [lab2] Build a Structural 3-bit Adder

--- lab2/src/full_adder.v
+++ lab2/src/full_adder.v
@@ -7,7 +7,16 @@ module full_adder (
 );
     // TODO: Insert your RTL here to calculate the sum and carry out bits
     // Remove these assign statements once you write your own RTL
-    assign sum = 1'b0;
-    assign carry_out = 1'b0;
+     wire a_and_b, a_and_c, b_and_c;
+     and(a_and_b, a, b);
+     and(a_and_c, a, carry_in);
+     and(b_and_c, b, carry_in);
 
+     wire carry_out_temp;
+     or(carry_out_temp, a_and_b, a_and_c);
+     or(carry_out, carry_out_temp, b_and_c);
+
+     wire sum_temp;
+     xor(sum_temp, a, b);
+     xor(sum, sum_temp, carry_in);
 endmodule
--- lab2/src/structural_adder.v
+++ lab2/src/structural_adder.v
@@ -5,6 +5,18 @@ module structural_adder (
 );
     // TODO: Insert your RTL here
     // Remove the assign statement once you write your own RTL
-    assign sum = 4'd0;
+
+    wire c[3:0];
+
+    assign c[0] = 0;
+    assign sum[3] = c[3];
+
+
+    genvar i;
+    generate
+        for(i = 0; i != 3; i = i + 1) begin:gen_adder
+            full_adder(a[i], b[i], c[i], sum[i], c[i+1]);
+        end
+    endgenerate
 
 endmodule
