f0a32a2db95975e10394ad8f8153cc1eafc1f3b8
RockRockWhite <1834309151@qq.com>
2024-04-09 16:56:38 +0800

    [Simulating Your Counter] finish

--- lab3/src/counter.v
+++ lab3/src/counter.v
@@ -1,15 +1,38 @@
 `include "../../lib/EECS151.v"
 
 module counter #(
-    parameter N = 4,
-    parameter RATE_HZ = 1
-) (
-    input clk,
-    input rst_counter,
-    input [N-1:0] rst_counter_val,
-    output [N-1:0] counter_output
-);
+        parameter N = 4,
+        parameter RATE_HZ = 1
+    ) (
+        input clk,
+        input rst_counter,
+        input [N-1:0] rst_counter_val,
+        output [N-1:0] counter_output
+    );
 
-    //TODO : Your code
+    // a reg to store the tick
+
+    wire [31:0] tick_cnt;
+    reg [31:0] tick_cnt_next;
+    reg [N-1:0] second_cnt;
+
+    REGISTER #(.N(32)) tick_reg(.q(tick_cnt), .d(tick_cnt_next), .clk(clk));
+
+    always @(*) begin
+        if(rst_counter) begin
+            tick_cnt_next = 0;
+            second_cnt = rst_counter_val;
+        end
+        else if(tick_cnt == RATE_HZ) begin
+            // a new second
+            tick_cnt_next = 0;
+            second_cnt = second_cnt + 1;
+        end
+        else begin
+            tick_cnt_next = tick_cnt + 1;
+        end
+    end
+
+    assign counter_output = second_cnt;
 
 endmodule
